302593a879f093ec739ef4013cdc6db8e545bbde
Suraj Bolanavar <svbsuraj@gmail.com>
2023-03-27 01:32:36 +0530

    Banner Implemented

--- src/components/HomeBanner.js
+++ src/components/HomeBanner.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./HomeBanner.css";
 
 const HomeBanner = () => {
   return (
-    <div>
-        <h1>Build your Wealth</h1>
-        <h6> When money realizes that it is in good hands, it wants to stay and multiply in those hands </h6>
+    <div className="banner__background">
+      <div className="banner__text-box">
+        <h1>Build Your Wealth</h1>
+        <h6>
+          When money realizes that it is in good hands, it wants to stay and
+          multiply in those hands
+        </h6>
         <p>-- Idowu Koyenikan</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeBanner
+export default HomeBanner;
